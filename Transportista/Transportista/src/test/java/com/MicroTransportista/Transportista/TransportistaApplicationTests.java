1b5dac33097e66bdbc92689cf3092ea2442fd8c5
Simone-userr <si.hercules@duocuc.cl>
2025-05-14 22:37:43 -0400

    microservicios Transportista

---
+++ Transportista/Transportista/src/test/java/com/MicroTransportista/Transportista/TransportistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MicroTransportista.Transportista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
