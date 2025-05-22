f6aa227aa885198edc8446e78dc04e7a663aeca2
Jayjowdev <ju.barber@duocuc.cl>
2025-05-22 10:27:19 -0400

    Add files via upload

---
+++ DetallesPedidos/src/main/java/com/AgroTech/DetallesPedidos/DetallesPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.AgroTech.DetallesPedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetallesPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetallesPedidosApplication.class, args);
+	}
+
+}
