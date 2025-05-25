244163a95ce8a975dfb8b2aefda83e51f79994bf
Juanjo Barber <ju.barber@duocuc.cl>
2025-05-25 02:51:28 -0400

    Solucion problema de ClienteClient y service

--- Pedidos/src/main/java/com/AgroTech/Pedidos/webclient/ClienteClient.java
+++ Pedidos/src/main/java/com/AgroTech/Pedidos/webclient/ClienteClient.java
@@ -8,17 +8,23 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class ClienteClient {
-
+    
     private final WebClient webClient;
 
-    public ClienteClient(@Value("${cliente-service.url}") String clienteServiceUrl) {
+    public ClienteClient(@Value ("${cliente-service.url}") String clienteServiceUrl) {
         this.webClient = WebClient.builder()
                 .baseUrl(clienteServiceUrl)
                 .build();
     }
 
-
-    public Map<String, Object> getClienteById(Long id) {
+    //devuelve un Map<String, Object>, es decir, una estructura tipo 
+    //clave:valor que representa los datos JSON del cliente.
+    public Map <String, Object> getClienteById(Long id) {
+        //vamos a hacer una solicitud HTTP de tipo GET
+        //retrieve --> ejecuta la solicitud y obtén la respuesta
+        //bodyToMono --ZAquí transformas el cuerpo de la respuesta en un objeto Java
+        //es un objeto de programación reactiva que representa un solo valor futuro
+        //block --> espera aquí hasta que llegue la respuesta
         return this.webClient.get()
             .uri("/{id}", id)
             .retrieve()
