bdfc72d90004e35d8ef0df038899bb8724cf3547
Jayjowdev <ju.barber@duocuc.cl>
2025-06-09 01:03:49 -0400

    Creación de microservicio de incidencia

---
+++ Incidencias/src/test/java/com/AgroTech/Incidencias/IncidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AgroTech.Incidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
